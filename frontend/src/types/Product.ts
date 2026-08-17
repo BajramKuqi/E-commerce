f8916d8a2773fa83209c771d18b1f8612f766dd7
bajramkuqi <bajramkuqi19@gmail.com>
2026-08-17 15:36:10 +0200

    fetch products from the api and show them

---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: number;
+    name: string;
+    description: string | null;
+    price: number;
+    stockQuantity: number;
+    categoryId: number;
+    categoryName: string;
+    createdAt: string;
+    rowVersion: number;
+}
